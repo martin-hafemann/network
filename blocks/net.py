3f071df5a3f0cc12d2ce08b8c7ed6eb809493847
mh <martin.hafemann@htwk-leipzig.de>
2023-09-06 11:14:27 +0200

    first commit

---
+++ blocks/net.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from pyomo.environ import *
+from pyomo.network import *
+
+# Get input parameters for the electrical net
+data = pd.read_csv(
+    'data/assets/electrical_net.csv',
+    index_col=0
+)
+
+
+def electrcial_net_block_rule(block):
+    """Rule for creating a electrical net block with default components and 
+    constraints."""
+    # Get index from model
+    t = block.model().t
+
+    # Define components
+    block.power = Var(t, domain=NonNegativeReals)
+
+    block.port_in = Port(initialize={'power': (block.power, Port.Extensive)})
+
+
+    # Define construction rules for constraints
+    def power_max_rule(_block, i):
+        power_max = data.loc['Max', 'Power']
+
+        return _block.power[i] <= power_max
+    
+
+    # Define constraints
+    block.power_max_constraint = Constraint(
+        t,
+        rule=power_max_rule
+        )
+    
+
+
+def gas_net_block_rule(block):
+    t = block.model().t
+
+    block.gas = Var(t, domain=NonNegativeReals)
+
+    block.port_out = Port(initialize={'gas': (block.gas, Port.Extensive)})
---
+++ main.py
@@ -0,0 +1,94 @@
+import pandas as pd
+
+from pyomo.environ import *
+from pyomo.network import *
+from pyomo.opt import SolverFactory
+
+import blocks.cgu as cgu
+import blocks.net as net
+
+
+PATH_PRICES = 'data/prices/'
+PATH_OUT = 'data/output/'
+
+opt = SolverFactory('gurobi')
+opt.options['nonconvex'] = 2
+
+
+data = DataPortal()
+
+data.load(
+        filename=PATH_PRICES + '/gas_price.csv',
+        index='t',
+        param='gas_price'
+)
+data.load(
+        filename=PATH_PRICES + '/power_price.csv',
+        index='t',
+        param='power_price'
+)
+
+m = AbstractModel()
+
+# Define sets
+m.t = Set(ordered=True)
+
+# Define parameters
+m.gas_price = Param(m.t)
+m.power_price = Param(m.t)
+
+# Define assets from blocks
+m.cgu1 = Block(rule=cgu.cgu_block_rule)
+m.cgu2 = Block(rule=cgu.cgu_block_rule)
+m.power_net = Block(rule=net.electrcial_net_block_rule)
+m.gas_net = Block(rule=net.gas_net_block_rule)
+
+
+def obj_expression(m):
+    """ Objective Function """
+    return (quicksum(m.gas_net.gas[t] * m.gas_price[t] for t in m.t) -
+            quicksum(m.power_net.power[t] * m.power_price[t] for t in m.t))
+
+
+m.obj = Objective(
+    rule=obj_expression,
+    sense=minimize
+    )
+
+# Create instance
+instance = m.create_instance(data)
+
+# Define arcs
+instance.arc1 = Arc(source=instance.cgu1.port_out, destination=instance.power_net.port_in)
+instance.arc2 = Arc(source=instance.cgu2.port_out, destination=instance.power_net.port_in)
+instance.arc3 = Arc(source=instance.cgu1.port_in, destination=instance.gas_net.port_out)
+instance.arc4 = Arc(source=instance.cgu2.port_in, destination=instance.gas_net.port_out)
+
+TransformationFactory('network.expand_arcs').apply_to(instance)
+
+# Solve the optimization problem
+results = opt.solve(
+    instance,
+    symbolic_solver_labels=True,
+    tee=True,
+    logfile='data/output/solver.log',
+    load_solutions=True)
+
+# Write Results
+results.write()
+
+df_variables = pd.DataFrame()
+df_parameters = pd.DataFrame()
+df_output = pd.DataFrame()
+
+for parameter in instance.component_objects(Param, active=True):
+    name = parameter.name
+    df_parameters[name] = [value(parameter[t]) for t in instance.t]
+
+for variable in instance.component_objects(Var, active=True):
+    name = variable.name
+    df_variables[name] = [value(variable[t]) for t in instance.t]
+
+df_output = pd.concat([df_parameters, df_variables], axis=1)
+
+df_output.to_csv('data/output/output_time_series.csv')
